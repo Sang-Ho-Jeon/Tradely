f35c66f2e19a486ea24c21d8c0a53e4b91cb63ef
Sang-Ho Jeon <us05255@naver.com>
2026-05-16 12:28:29 +0900

    add server-url env and config swagger

--- src/main/java/com/sysmatic2/finalbe/config/SwaggerConfig.java
+++ src/main/java/com/sysmatic2/finalbe/config/SwaggerConfig.java
@@ -5,16 +5,20 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${app.server-url}")
+    private String serverUrl;
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .addServersItem(new Server().url("https://api.2ven.shop")) // HTTPS 사용
+                .addServersItem(new Server().url(serverUrl)) // HTTPS 사용
                 .components(new Components()
                         .addSecuritySchemes("BearerAuth",
                                 new SecurityScheme()
